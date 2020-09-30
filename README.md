7fecaa4a2b6052620964e5bdc0cef986174827ce
Frank Leng <email@frankleng.me>
2020-09-29 17:54:58 -0700

    v1

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# classname-prefix-loader
+# tailwind-classname-prefix-loader
+
+Add a prefix to all Tailwind classes in a `className` (React default) attribute.
+
+Usage Example
+```js
+//webpack.config.js
+  {
+    test: /\.(js|mjs|jsx|ts|tsx)$/,
+    use: [
+      {
+        loader: 'babel-loader',
+        options: {
+          ...
+        }
+      },
+      {
+        loader: 'node_modules/tailwind-classname-prefix-loader/lib/index.js',
+        options: {
+          prefix: 'prefix-',
+          attrs: [
+            // additional attributes to prefix
+
+            // ie. transition classes from React Transition Group component
+            'enter',
+            'enterFrom',
+            'enterTo',
+            'leave',
+            'leaveFrom',
+            'leaveTo',
+
+            // ie. CSS Module
+           'styleName'
+          ]
+        }
+      }
+    ]
+  }
+```
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import tailwindClasses from './tailwindClasses';
+import * as webpack from 'webpack';
+
+const prefixCache: { [x: string]: string } = {};
+
+function applyPrefix(prefix: string, snippet: string) {
+  if (prefixCache[snippet]) return prefixCache[snippet];
+
+  let result = snippet;
+
+  // credit: https://github.com/vesper8/vue-tailwind-prefix-applicator/blob/master/src/views/Home.vue
+  const escapeRegExp = (s: string) => s.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
+  tailwindClasses.forEach((cls) => {
+    result = result.replace(
+      new RegExp(`(["':\\s])(?!${prefix})(-?${escapeRegExp(cls)})(?![-/])`, 'g'),
+      `$1${prefix}$2`,
+    );
+  });
+
+  prefixCache[snippet] = result;
+  return result;
+}
+
+function loader(this: webpack.loader.LoaderContext, content: string) {
+  const { prefix, attrs } = this.query;
+  let classNameAttrs = ['className'];
+  if (attrs) {
+    classNameAttrs = classNameAttrs.concat(attrs);
+  }
+
+  const classMatchRegex = new RegExp(`(${classNameAttrs.join('|')})=\"(.+)\"`, 'gim');
+  const classNamesMatches = content.match(classMatchRegex);
+
+  if (classNamesMatches) {
+    for (const match of classNamesMatches) {
+      const prefixed = applyPrefix(prefix, match);
+      content = content.replace(match, prefixed);
+    }
+  }
+  return content;
+}
+
+export default loader;
